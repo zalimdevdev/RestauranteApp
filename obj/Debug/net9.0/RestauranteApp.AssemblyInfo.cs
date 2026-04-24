6eb02e0cd2618c12b4902a0e12eec681282546f2
zalimdevdev <zalimdevdev@hotmail.com>
2026-04-24 00:22:40 -0600

    facturafix2

--- obj/Debug/net9.0/RestauranteApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/RestauranteApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestauranteApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e923c41c9f1a7bb86712c47b9027220de7f123d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a2d8e4fa588999d2caed75740f8af7d9b6324bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestauranteApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestauranteApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
